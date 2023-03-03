2f201e11eca5d0242b2416873e4e31ec7160ce47
Artur Sampaio <arturcedaspy@gmail.com>
2023-03-03 15:33:18 -0300

    Initial commit

---
+++ app/Http/Controllers/API/LoginController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function loginAuthenticate(Request $request)
+    {
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $user = Auth::user();
+            $token = $user->createToken('JWT');
+
+            return response()->json($token->plainTextToken, 200);
+        }
+
+        return response()->json('Usuario invalido', 401);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\API\LoginController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    $user = Auth::user();
+
+    return response()->json($user, 200);
+});
+
+Route::post('/login', [LoginController::class, 'loginAuthenticate']);
